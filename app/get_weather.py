35c72da75711c4405cec3dcbc6134e5a964532db
MJ Rossetti <datacreativellc@gmail.com>
2017-07-22 18:35:43 -0400

    Issue request for weather in New Haven

--- app/get_weather.py
+++ app/get_weather.py
@@ -5,7 +5,7 @@ import json
 # COMPILE QUERY
 
 baseurl = "https://query.yahooapis.com/v1/public/yql?"
-yql_query = "select wind from weather.forecast where woeid=2460286"
+yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='new haven, ct')"
 yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
 
 # ISSUE REQUEST
@@ -13,7 +13,3 @@ yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
 result = urllib.request.urlopen(yql_url).read()
 data = json.loads(result)
 print(data['query']['results'])
-
-
-
-# select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="new haven, ct")
